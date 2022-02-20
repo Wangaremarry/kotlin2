6d6c3cdda42610bd7129d783c2d743852298bf76
wangare <marywangareee@gmail.com>
2022-02-20 22:06:00 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,21 @@
+fun main(){
+    surname( "Mary")
+    modules(26, 34)
+    add(30, 60,47,57)
+    myfact()
+
+}
+fun surname(name:String){
+    println( "Hello " +name)
+}
+fun modules(a:Int, b:Int){
+    var modules=a%b
+    println(modules)
+}
+fun add(c:Int, d:Int, e:Int, f:Int){
+    var sum=(c+d+e+f)
+    println(sum)
+}
+fun myfact(){
+    println("I am responsible")
+}
